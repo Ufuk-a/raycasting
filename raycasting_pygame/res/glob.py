26d82f46ddb9970ab6ee0874f289558e1c811227
Ufuka <61376126+Ufuk-a@users.noreply.github.com>
2020-11-26 12:57:54 +0300

    Add files via upload

--- raycasting_pygame/res/glob.py
+++ raycasting_pygame/res/glob.py
@@ -1,1 +1,17 @@
+import pygame 
+import math as m
+pygame.init()
 
+WIDTH, HEIGHT = 600, 600
+TITLE = "Raycasting"
+FPS = 60
+
+class colors:
+    red = (255, 0, 0)
+    black = (0, 0, 0)
+    blue = (0, 0, 255)
+    lime = (0, 255, 0)
+    turq = (64, 224, 208)
+    orange = (255, 69, 0)
+    white = (255, 255, 255)
+    green = (34, 139, 34)
